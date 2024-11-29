d0131b6dc3f5ebadfc94fefbd24e2dcdba2aefd0
CarloDettori <dettoric07@gmail.com>
2024-11-29 15:16:41 +0100

    done hobbies code

---
+++ hobbies.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree]
+};
+
+console.log(hobbies("fishing", "hiking", "snorkeling"));
+
+module.export = hobbies;
